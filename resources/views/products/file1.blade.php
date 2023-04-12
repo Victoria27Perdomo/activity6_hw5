6e2fb13bca3efd93407c7140d46252f0d4da3c6c
VICTORIA PERDOMO <victoria27perdomo@gmail.com>
2023-04-11 19:57:54 -0600

    Base Products

---
+++ resources/views/products/file1.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Crimson+Pro">
+    <style>
+      body {
+        font-family: 'Crimson Pro', serif;
+        font-size: 48px;
+      }
+    </style>
+  </head>
+  <body>
+      <div>Product View</div>
+  </body>
+</html>
